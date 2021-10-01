b859c311046aba85cf78423dd6df49cfdf18bc1c
Alecia113 <70254813+Alecia113@users.noreply.github.com>
2021-10-01 21:15:55 +1000

    Update dmoz_spider.py

--- DmoTest/DmoTest/spiders/dmoz_spider.py
+++ DmoTest/DmoTest/spiders/dmoz_spider.py
@@ -14,17 +14,18 @@ class DmozSpider(scrapy.Spider):
     #Separate the category
     def parse(self,response):          #parse is the default method for handling responses.
         items = []
-        Category = response.xpath('//div[@class="outer-cats"]//h2/a/@href').extract()
+        Category = response.xpath('//div[@class="outer-cats"]//h2/a/@href').extract() # If you don't want ' / ', you can try: Category =response.xpath('//div[@class="outer-cats"]//h2/a//text()').extract()
         for category in range(len(Category)):
             item = []
             item = DmozItem()
             Every_category = []
             Every_category.append(Category[category])
             item['Every_category'] = Every_category
-            item['Category'] = response.xpath('//div[@class="outer-cats"]//h2/a/@href').extract()
+            # item['Category'] = Category
+            item['Category'] = response.xpath('//div[@class="outer-cats"]//h2/a/@href').extract() # item['Category'] =response.xpath('//div[@class="outer-cats"]//h2/a//text()').extract()
             item['Website_name'] = response.selector.xpath('//title/text()').extract_first()
             item['Website_URL'] = response.url
-            item['Website_title'] = response.xpath('//div[@class="outer-cats"]//a/@href').extract()
+            item['Website_title'] = response.xpath('//div[@class="outer-cats"]//a/@href').extract()   # item['Website_title'] =response.xpath('//div[@class="outer-cats"]//a//text()').extract()
             items.append(item)
         return items
 
